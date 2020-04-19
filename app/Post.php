2f21541f0720c9da62c68fea3858eabf28ab4023
Jackson Gonzalez <34948692+JacksonGonzalez@users.noreply.github.com>
2020-04-19 14:00:47 -0500

    ejemplo de como transformar datos desde la entidad

--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+   /* protected $appends = ['post_excerpt'];
+
+
+    public function getPostExcerptAttribute()
+    {
+        return strtolower(
+            substr($this->body, 0, 100)
+        ).'...';
+    }*/
 }
